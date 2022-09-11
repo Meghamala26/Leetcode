c01b5e6eb216ffd100ef33d1ee2211be346f46d4
Meghamala Gupta <56683039+Meghamala26@users.noreply.github.com>
2022-09-11 14:45:11 -0400

    Time: 263 ms (39.83%), Space: 13.9 MB (75.92%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.py
+++ 647-palindromic-substrings/647-palindromic-substrings.py
@@ -1,29 +1,23 @@
 class Solution:
     
     
-    def expandAroundCenter(self,s, left, right, found):
+    def expandAroundCenter(self,s, left, right):
             count=0
             
             while(left>=0 and right < len(s) and s[left]==s[right]):
-                tmp=str(left)+','+str(right)
-                if tmp not in found:
-                    found.add(tmp)
                     left-=1
                     right+=1
                     count+=1
-                else:
-                    break
                 
             return count
         
     def countSubstrings(self, s: str) -> int:
         
-        found=set()
         res=0
         
         for i in range(len(s)):
-            res+=self.expandAroundCenter(s,i,i, found)
-            res+=self.expandAroundCenter(s,i,i+1, found)
+            res+=self.expandAroundCenter(s,i,i)
+            res+=self.expandAroundCenter(s,i,i+1)
         
         
         return res
