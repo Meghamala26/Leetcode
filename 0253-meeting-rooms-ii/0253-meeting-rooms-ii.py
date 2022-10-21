32088e1103f40cc02f34ddcd91c8270e6452449f
Meghamala Gupta <56683039+Meghamala26@users.noreply.github.com>
2022-10-21 18:36:46 -0400

    Time: 54 ms (97.89%), Space: 17.1 MB (23.02%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
@@ -6,27 +6,22 @@ class Solution(object):
         """
         
         intervals=sorted(intervals, key=lambda x:x[0])
-        print(intervals)
-        rooms=[intervals[0]]
+        #print(intervals)
+        rooms=[]
+        heapq.heappush(rooms, intervals[0][1])
         for x in intervals[1:]:
             start=x[0]
             end=x[1]
-            flag=0
-            #print(x)
-            #print(rooms)
-            for y in rooms:
-                rstart=y[0]
-                rend=y[1]
-                if (start<=rstart and end<=rstart) or (start>=rend and end>=rend):
-                    #print(1)
-                    y[0]=min(start,rstart)
-                    y[1]=max(end,rend)
-                    flag=1
-                    break
+           
+            if rooms[0]<=start:
+                heapq.heappop(rooms)
+                
+            heapq.heappush(rooms, end)
+                
+                
+
                     
-            if flag==0: 
-                #print(0)
-                rooms.append([start,end])
+            
                 
         return len(rooms)
                     
