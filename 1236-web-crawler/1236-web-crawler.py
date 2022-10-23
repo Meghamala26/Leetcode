7eb52e97b050035da43f96a7612bec0ad36592e4
Meghamala Gupta <56683039+Meghamala26@users.noreply.github.com>
2022-10-23 14:57:22 -0400

    Time: 461 ms (58.92%), Space: 20.7 MB (53.75%) - LeetHub

---
+++ 1236-web-crawler/1236-web-crawler.py
@@ -0,0 +1,49 @@
+# """
+# This is HtmlParser's API interface.
+# You should not implement it, or speculate about its implementation
+# """
+#class HtmlParser(object):
+#    def getUrls(self, url):
+#        """
+#        :type url: str
+#        :rtype List[str]
+#        """
+
+class Solution:
+    def crawl(self, startUrl: str, htmlParser: 'HtmlParser') -> List[str]:
+        
+        
+        
+        def getHostName(url):
+            hostname=''
+            i=0
+            while(i<len(url)):
+                hostname+=url[i]
+                i+=1
+                if hostname=='http://':
+                    break
+                
+            hostname=''
+            while(i<len(url)):
+                if url[i] == '.' or url[i]=='-' or url[i] in "abcdefghijklmnopqrstuvwxyz" or url[i] in "0123456789":
+                    hostname+=url[i]
+                else:
+                    break
+                i+=1
+            return hostname
+                
+        visited = set()
+        q = [startUrl]
+        visited.add(startUrl)
+        res = []
+        
+        while q:
+            url = q.pop(0)
+            res.append(url)
+            
+            for n in htmlParser.getUrls(url):
+                if getHostName(n) == getHostName(url) and n not in visited:
+                    q.append(n)
+                    visited.add(n)
+        return res
+            
