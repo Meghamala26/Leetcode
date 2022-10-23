0b5c360b0f85607766765c607d427077386f42c8
Meghamala Gupta <56683039+Meghamala26@users.noreply.github.com>
2022-10-22 20:30:46 -0400

    Time: 202 ms (88.11%), Space: 14 MB (87.19%) - LeetHub

--- 0723-candy-crush/0723-candy-crush.py
+++ 0723-candy-crush/0723-candy-crush.py
@@ -1,28 +1,25 @@
 class Solution:
     def candyCrush(self, board: List[List[int]]) -> List[List[int]]:
         
-        R, C = len(board), len(board[0])
-        todo = False
+        M=board
+        while True:
+        # 1, Check
+            crush = set()
+            for i in range(len(M)):
+                for j in range(len(M[0])):
+                    if j > 1 and M[i][j] and M[i][j] == M[i][j - 1] == M[i][j - 2]:
+                        crush |= {(i, j), (i, j - 1), (i, j - 2)}
+                    if i > 1 and M[i][j] and M[i][j] == M[i - 1][j] == M[i - 2][j]:
+                        crush |= {(i, j), (i - 1, j), (i - 2, j)}
 
-        for r in range(R):
-            for c in range(C-2):
-                if abs(board[r][c]) == abs(board[r][c+1]) == abs(board[r][c+2]) != 0:
-                    board[r][c] = board[r][c+1] = board[r][c+2] = -abs(board[r][c])
-                    todo = True
+            # 2, Crush
+            if not crush: break
+            for i, j in crush: M[i][j] = 0
 
-        for r in range(R-2):
-            for c in range(C):
-                if abs(board[r][c]) == abs(board[r+1][c]) == abs(board[r+2][c]) != 0:
-                    board[r][c] = board[r+1][c] = board[r+2][c] = -abs(board[r][c])
-                    todo = True
-
-        for c in range(C):
-            wr = R-1
-            for r in range(R-1, -1, -1):
-                if board[r][c] > 0:
-                    board[wr][c] = board[r][c]
-                    wr -= 1
-            for wr in range(wr, -1, -1):
-                board[wr][c] = 0
-
-        return self.candyCrush(board) if todo else board
+            # 3, Drop
+            for j in range(len(M[0])):
+                idx = len(M) - 1
+                for i in reversed(range(len(M))):
+                    if M[i][j]: M[idx][j] = M[i][j]; idx -= 1
+                for i in range(idx + 1): M[i][j] = 0
+        return M
