a3827a03a22a483b6dd4c624d4076a7da231741a
Meghamala Gupta <56683039+Meghamala26@users.noreply.github.com>
2022-11-13 19:40:26 -0500

    Time: 1543 ms (9.58%), Space: 17.1 MB (67.53%) - LeetHub

--- 0826-most-profit-assigning-work/0826-most-profit-assigning-work.py
+++ 0826-most-profit-assigning-work/0826-most-profit-assigning-work.py
@@ -1,17 +1,41 @@
 class Solution:
     def maxProfitAssignment(self, difficulty: List[int], profit: List[int], worker: List[int]) -> int:
         
+        n=len(difficulty)
+        m=len(worker)
+        jobs=[]
+        for i in range(n):
+            jobs.append((difficulty[i],profit[i]))
         
-        jobs=sorted(zip(difficulty,profit))
-        maxProfit =bestMatch= j=0
-        for skill in sorted(worker):
-            while(j<len(jobs) and skill>=jobs[j][0]):
-                bestMatch=max(bestMatch,jobs[j][1] )
-                j+=1
-            maxProfit+=bestMatch
+        jobs.sort()
+        
+        maxProfitSoFar=[0]*n
+        maxProfitSoFar[0]=jobs[0][1]
+        
+        for i in range(1,n):
+            maxProfitSoFar[i]=max(maxProfitSoFar[i-1],jobs[i][1])
+        
+        maxProfit=0
+        for skill in worker:
+            
+            bestIndex=-1
+            start=0
+            end=n-1
+            
+            while(start<=end):
+                mid=start+(end-start)//2
                 
+                if jobs[mid][0]<=skill:
+                    bestIndex=mid
+                    start=mid+1
+                else:
+                    end=mid-1
+            
+            if bestIndex!=-1:
+                maxProfit+=maxProfitSoFar[bestIndex]
+        
         return maxProfit
-                    
+                
             
             
             
