7c0f44788bf0d27fea2b642fd4c78c08d12e54c0
Meghamala Gupta <56683039+Meghamala26@users.noreply.github.com>
2022-12-06 11:47:04 -0500

    Time: 2306 ms (69.88%), Space: 43.6 MB (6.30%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -2,25 +2,23 @@ class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         
         memo={0:0}
-        
         def getMinCoins(amnt):
+            
             if amnt<0:
                 return -1
             if amnt in memo:
                 return memo[amnt]
             
-            minCost=float('inf')
-            
+            minCoins=float('inf')
             for x in coins:
                 res=getMinCoins(amnt-x)
                 if res!=-1:
-                    minCost=min(minCost,res+1)
+                    minCoins=min(minCoins,res+1)
             
-            memo[amnt]=minCost if minCost!=float('inf') else -1
+            memo[amnt]=minCoins if minCoins!=float('inf') else -1
             return memo[amnt]
-                
         
         getMinCoins(amount)
         return memo[amount]
-                
+                    
         
