a8737fda45eac147a9afb7d48efa2505b8d5a9c9
Meghamala Gupta <56683039+Meghamala26@users.noreply.github.com>
2023-01-06 18:49:12 -0500

    Time: 1710 ms (69.37%), Space: 43.5 MB (7.70%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -2,10 +2,11 @@ class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         
         memo={0:0}
+        
         def getMinCoins(amnt):
-            
             if amnt<0:
                 return -1
+            
             if amnt in memo:
                 return memo[amnt]
             
@@ -14,11 +15,13 @@ class Solution:
                 res=getMinCoins(amnt-x)
                 if res!=-1:
                     minCoins=min(minCoins,res+1)
+                    
             
             memo[amnt]=minCoins if minCoins!=float('inf') else -1
             return memo[amnt]
-        
+    
         getMinCoins(amount)
         return memo[amount]
-                    
+            
+            
         
