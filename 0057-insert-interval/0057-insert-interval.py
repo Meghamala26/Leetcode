a6c9b1a088525fa0da911a70b46e8cf724738f1f
Meghamala Gupta <56683039+Meghamala26@users.noreply.github.com>
2023-01-15 14:38:54 -0500

    Time: 82 ms (87.16%), Space: 17.3 MB (91.44%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -1,39 +1,47 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        if len(intervals)==0:
-            return [newInterval]
-        
-        output=[]
-        idx = bisect.bisect_left(intervals,newInterval)
-        
-        output=intervals[:idx]
-        
-        for i in range(idx-1,len(intervals)):
-            #lastStart,lastEnd=output[-1][0],output[-1][1]
-            
-            if i==idx-1:
-                #handle newInterval
-                if output and output[-1][0]<=newInterval[0]<=output[-1][1] :
-                    output[-1][1]=max(output[-1][1],newInterval[1])
+        new_interval=newInterval
+        existing_intervals=intervals
+        if intervals==[]:
+            return [new_interval]
+        result=[]
+        def findPos():
+            low,high=0,len(existing_intervals)-1
+            closest=float('-inf')
+            while(low<=high):
+                mid=(low+high)//2
+                if existing_intervals[mid][0]<=new_interval[0]:
+                    closest=max(closest,mid)
+                    low=mid+1
                 else:
-                    output.append(newInterval)
-                    
+                    high=mid-1
+            return closest
+
+        def mergeInterval(new):
+            if new[0]<=result[-1][1]:
+                result[-1][1]=max(new[1],result[-1][1])
             else:
-                #handle existing Intervals
-                if output and output[-1][0]<=intervals[i][0]<=output[-1][1]:
-                    output[-1][1]=max(output[-1][1],intervals[i][1])
+                result.append(new)
+
+        if new_interval[0]<=existing_intervals[0][0]:
+            result.append(new_interval)
+            pos=-2
+        else:
+            pos=findPos()
+        
+        print(pos)
+        if pos==len(existing_intervals)-1:
+            result=existing_intervals
+            mergeInterval(new_interval)
+        else:
+            for i in range(len(existing_intervals)):
+                if i<=pos:
+                    result.append(existing_intervals[i])
+                elif i==pos+1:
+                    mergeInterval(new_interval)
+                    mergeInterval(existing_intervals[i])
                 else:
-                    output.append(intervals[i])
-                    
-        return output
-                
-                
-            
-            
-            
-                
-            
-            
-        
-        
-        
+                    mergeInterval(existing_intervals[i])
+
+
+        return result
