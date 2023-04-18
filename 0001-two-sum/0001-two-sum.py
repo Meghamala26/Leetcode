9962531dd2f351608fa7cbea87a00a78016c453e
Meghamala Gupta <56683039+Meghamala26@users.noreply.github.com>
2023-04-17 20:09:02 -0400

    Time: 66 ms (57.92%), Space: 15.1 MB (36.03%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,9 +1,11 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         
-        idxMap = {nums[i] : i for i in range(len(nums))}
+        idxMap={}
         for i in range(len(nums)):
-            if (target - nums[i]) in idxMap and idxMap[(target - nums[i])]!=i:
-                return [i,idxMap[target - nums[i]]]
+            if (target-nums[i]) in idxMap:
+                return [i,idxMap[(target-nums[i])]]
+            else:
+                idxMap[nums[i]]=i
         
         
